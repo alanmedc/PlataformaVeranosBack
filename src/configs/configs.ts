b003045baad6f3d5715ce944e6ba91a863013570
JaviZamudio <javier.elihu100@gmail.com>
2023-09-01 13:28:42 -0600

    DATABASE EN CONFIG

--- src/configs/configs.ts
+++ src/configs/configs.ts
@@ -3,3 +3,4 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT || 3000;
+export const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:27017/express-typescript";
