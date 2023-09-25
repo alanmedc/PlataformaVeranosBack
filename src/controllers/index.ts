fd55c712c83a5d8d875fe1d13e4a7e9fb82430f6
Omar Valadez <omarvaladz@gmail.com>
2023-09-25 14:17:51 -0600

    Creación de grupo a partir de una solicitud si no existe con una materia

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -16,15 +16,43 @@ export async function createRequest(req: Request, res: Response) {
     try {
         const requestData = req.body;
 
-        const requiredFields = ['id_solicitud', 'expediente_alumno', 'nombre_alumno', 'ap_paterno', 'ap_materno', 'email_alumno', 'id_grupo'];
+        const requiredFields = ['expediente_alumno', 'nombre_alumno', 'ap_paterno', 'ap_materno', 'email_alumno', 'clave_materia'];
 
         for (const field of requiredFields) {
             if (!requestData.hasOwnProperty(field) || requestData[field] === null || requestData[field] === undefined) {
                 return res.status(400).json({ error: `El campo '${field}' es obligatorio` });
             }
         }
+
+        const existingGroup = await prisma.grupo.findFirst({
+            where: {
+                clave_materia: requestData.clave_materia,
+            },
+        });
+
+        let groupId;
+
+        if (existingGroup) {
+            groupId = existingGroup.id_grupo;
+        } else {
+            const newGroup = await prisma.grupo.create({
+                data: {                    
+                    clave_materia: requestData.clave_materia,
+                },
+            });
+            groupId = newGroup.id_grupo;
+        }
+        
         const newRequest = await prisma.solicitud.create({
-            data: requestData,
+            data: {
+                id_solicitud: 3,
+                expediente_alumno: requestData.expediente_alumno,
+                nombre_alumno: requestData.nombre_alumno,
+                ap_paterno: requestData.ap_paterno,
+                ap_materno: requestData.ap_materno,
+                email_alumno: requestData.email_alumno,
+                id_grupo: groupId,
+            },
         });
 
         return res.json(newRequest);
