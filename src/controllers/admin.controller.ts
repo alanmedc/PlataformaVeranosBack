b420426af4028465091206641cad9d19bac53761
Omar Valadez <omarvaladz@gmail.com>
2023-10-02 10:51:53 -0600

    Se agrega función para modificar información de un grupo en específico mediante una petición PUT

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import  jwt  from 'jsonwebtoken';
+import jwt from 'jsonwebtoken';
 
 import bcrypt from 'bcrypt';
 import { JWT_SECRET } from "../configs/configs";
 const prisma = new PrismaClient();
 
-export const adminLogin = async (req: Request, res: Response)=> {
+export const adminLogin = async (req: Request, res: Response) => {
 
     //POR SI HAY QUE ACTUALIZAR ALGUN CAMPO EN LA BASE DE DATOS
     // const newPassword = bcrypt.hashSync(req.body.password, 10);
@@ -21,33 +21,60 @@ export const adminLogin = async (req: Request, res: Response)=> {
     // return res.send("todo bien");
 
     try {
-        const {username, password} = req.body;
-        if(!username || !password){
-            return res.status(400).json({error: 'Faltan datos.'});
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({ error: 'Faltan datos.' });
         }
-        
+
         const admin = await prisma.admin.findFirst({
             where: {
                 username: username,
             },
         });
 
-        if(!admin){
-            return res.status(404).json({message: "Admin no encontrado."});
+        if (!admin) {
+            return res.status(404).json({ message: "Admin no encontrado." });
         }
 
-        const isValidPass = bcrypt.compareSync(password, admin.password);
+        const isValidPass = bcrypt.compareSync(password, admin.password || "");
 
-        if(!isValidPass){
-            return res.status(400).json({message: "Contraseña no valida."});
+        if (!isValidPass) {
+            return res.status(400).json({ message: "Contraseña no valida." });
         }
 
-        const token = jwt.sign({id: admin.id_admin}, JWT_SECRET);
-        
-        return res.json({message: "Login correcto.",
-                data: token});
+        const token = jwt.sign({ id: admin.id_admin }, JWT_SECRET);
 
-    }catch (error) {
-        return res.status(500).json({message: "Algo salió mal."});
+        return res.json({
+            message: "Login correcto.",
+            data: token
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "Algo salió mal." });
+    }
+}
+
+export async function updateGroup(req: Request, res: Response) {
+    try {
+        const groupId = parseInt(req.params.id, 10);
+        const updatedData = req.body;
+
+        const existingGroup = await prisma.grupo.findUnique({
+            where: { id_grupo: groupId },
+        });
+
+        if (!existingGroup) {
+            return res.status(404).json({ error: 'El grupo no existe' });
+        }
+
+        const updatedGroup = await prisma.grupo.update({
+            where: { id_grupo: groupId },
+            data: updatedData,
+        });
+
+        return res.status(200).json({ message: 'Grupo actualizado correctamente', data: updatedGroup });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error al actualizar el grupo' });
     }
 }
