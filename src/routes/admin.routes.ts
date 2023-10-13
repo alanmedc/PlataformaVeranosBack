46fc437ca41abb4b0fe2c7721df6a58204decf54
Alan Medina <alanmedc98@gmail.com>
2023-10-13 15:28:54 -0600

    Información de grupo para el admin.

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { getAdmins } from "../controllers";
 import { auth } from "../middlewares/auth";
-import { adminLogin, updateGroup } from "../controllers/admin.controller";
+import { adminLogin, updateGroup, groupInfo } from "../controllers/admin.controller";
 
 
 const adminRouter = Router();
 
 adminRouter.get('/', auth, getAdmins);
+adminRouter.get('/groups/:id', groupInfo);
 adminRouter.post('/login', adminLogin);
 adminRouter.put('/updateGroup/:id', updateGroup);
 
